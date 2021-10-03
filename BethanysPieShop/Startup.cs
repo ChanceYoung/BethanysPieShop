bb1c6f642c5ec47b98eed5d297ec95c2b097b8d0
Chance Young <chancethehackerv2@gmail.com>
2021-10-02 19:32:23 -0600

    Created The home page, added partial views and began adding the ShoppingCart functionality

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -29,6 +29,8 @@ namespace BethanysPieShop
             services.AddScoped<IPieRepository, PieRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddControllersWithViews();
+            services.AddHttpContextAccessor();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +45,7 @@ namespace BethanysPieShop
             app.UseStaticFiles();
             
             app.UseRouting();
-
+            app.UseSession();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
